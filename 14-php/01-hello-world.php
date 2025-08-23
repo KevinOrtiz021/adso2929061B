46e4c34e5512a0ed676af019d3794623b05fbe80
ArenasKevin021 <arenaskevin021@gmail.com>
2025-08-22 21:04:30 -0500

    php

---
+++ 14-php/01-hello-world.php
@@ -0,0 +1,9 @@
+<?php
+    $title = '01-Hello World';
+    $description = 'Lorem ipsum dolor';
+    include 'template/header.php';
+    echo "<section>";
+    echo "<h3>Hello World PHP!</h3>";
+    print('<p style="text-align: justify">Lorem Input dolor sit amet consectetur adipisicing elit.</p>');
+
+    include 'template/footer.php';
---
+++ 14-php/03-comments.php
@@ -0,0 +1,21 @@
+<?php
+    $title = '03-Comments';
+    $description = 'Allow comments in code.';
+
+    include 'template/header.php';
+
+    echo "<section>";
+    
+    // Line is a LineComments
+    // echo "<h3>Show Code</h3>";
+    /*
+        Multi-LIne Comment
+        hi, Hello, :)
+        This is a line jijiji
+        echo "Line 4";
+        echo "Line 5";
+    */
+
+    # This is another line comment
+
+    include 'template/footer.php';
