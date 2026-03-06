2bda5c29788bd6399f2ab79e1c4f87567fee16e7
Arenaskevin021 <Arenaskevin021@gmail.com>
2026-03-06 18:53:29 -0500

    registrer

--- 19-laravel/resources/views/auth/register.blade.php
+++ 19-laravel/resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,54 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+@extends('layouts.app')
+
+@section('title', 'Larapets: Login')
+@section('content')
+
+    <section class="bg-[#0006] p-4 border-white border-2 rounded-md w-90 flex flex-col justify-center items-center">
+
+        <h1 class="text-4xl flex border-b-2 pb-2 gap-2 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-10" fill="currentColor" viewBox="0 0 256 256"><path d="M256,136a8,8,0,0,1-8,8H232v16a8,8,0,0,1-16,0V144H200a8,8,0,0,1,0-16h16V112a8,8,0,0,1,16,0v16h16A8,8,0,0,1,256,136Zm-57.87,58.85a8,8,0,0,1-12.26,10.3C165.75,181.19,138.09,168,108,168s-57.75,13.19-77.87,37.15a8,8,0,0,1-12.25-10.3c14.94-17.78,33.52-30.41,54.17-37.17a68,68,0,1,1,71.9,0C164.6,164.44,183.18,177.07,198.13,194.85ZM108,152a52,52,0,1,0-52-52A52.06,52.06,0,0,0,108,152Z"></path></svg>
+            Register
+        </h1>
+        {{-- -Register Form --}}
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            {{-- -First column --}}
+            <div class="w-60 text-white">
+                {{-- Document --}}
+                <label for="label">Document:</label>
+                <input class="input bg-[#0009] outline-1" type="text" name="document" value="{{ old('document') }}"
+                placeholder="7500000001">
+                @error('document')
+                    <small class="badge badge-error w-full ">{{ $message }} </small>
+                @enderror
+
+            {{-- Fullname --}}
+                <label for="label">Fullname:</label>
+                <input class="input bg-[#0009] outline-1" type="text" name="fullname" value="{{ old('fullname') }}"
+                placeholder="Jeremias Sprinfield">
+                @error('fullname')
+                    <small class="badge badge-error w-full ">{{ $message }} </small>
+                @enderror
+                {{-- Gender --}}
+                <label for="label">Gender:</label>
+                <select name="gender" class="select bg-[#0009] outline-1">
+                    <option value="">Select...</option>
+                    <option value="Female" @if (old('gender') == 'Female') selected @endif>Female</option>
+                    <option value="Male"@if (old('gender') == 'Male') selected @endif>Male</option>
+                </select>
+
+                @error('genders')
+                    <small class="badge badge-error w-full ">{{ $message }} </small>
+                @enderror
+            </div>
+            {{-- Seconds column --}}
+            <div class="w-60">
+
+            </div>
+        </form>
+    </section>
+@endsection
--- 19-laravel/resources/views/layouts/app.blade.php
+++ 19-laravel/resources/views/layouts/app.blade.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
+    <title>@yield('tittle')</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+
 </head>
-<body class="bg-[linear-gradient(to_top,#000c,#0000),url(images/bg-welcome.jpg)] bg-center bg-no-repeat bg-cover text-white min-h-dvh flex flex-col gap-2 justify-center items-center">
+
+<body
+    class="bg-[linear-gradient(to_top,#000a,#000a),url('images/bg-welcome.jpg')] min-h-dvh flex flex-col gap-2 justify-center items-center bg-no-repeat bg-center bg-cover">
+
     <main>
         @yield('content')
     </main>
+
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
     <script>
         @yield('js')
     </script>
+
 </body>
+
 </html>
